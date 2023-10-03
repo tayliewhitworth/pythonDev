76485f85200acbb478e54ea88c61a2192ff39caa
Taylie Whitworth <taylie.carlson@gmail.com>
2023-10-03 14:32:21 -0600

    FIrst commit

---
+++ ShoppingCart.py
@@ -0,0 +1,182 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        # Initialize product attributes
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+class ShoppingCart:
+    def __init__(self):
+        # Initialize shopping cart attributes
+        self.products = []
+        self.tax_rate = 0.0
+
+    def add_product(self, product):
+        # Add a product to the cart
+        self.products.append(product)
+
+    def remove_product(self, product_name):
+        # Remove a product from the cart by name
+        product_to_remove = None
+        for product in self.products:
+            if product.name == product_name:
+                product_to_remove = product
+                break
+        if product_to_remove is not None:
+            self.products.remove(product_to_remove)
+        else:
+            print("Product not found in cart.")
+
+    def update_quantity(self, product_name, new_quantity):
+        # Update the quantity of a product in the cart
+        product_to_update = None
+        for product in self.products:
+            if product.name == product_name:
+                product_to_update = product
+                break
+        if product_to_update is not None:
+            product_to_update.quantity = new_quantity
+        else:
+            print("Product not found in cart.")
+
+    def calculate_subtotal(self):
+        # Calculate the subtotal of items in the cart
+        subtotal = 0
+        if self._is_cart_empty():
+            return 0
+        for product in self.products:
+            subtotal += product.price * product.quantity
+        return subtotal
+
+    def set_tax_rate(self, tax_rate):
+        # Set the tax rate for the cart
+        if not (isinstance(tax_rate, float) and 0 <= tax_rate <= 1):
+            raise ValueError("Tax rate must be a decimal between 0 and 1.")
+        self.tax_rate = tax_rate
+
+    def calculate_tax(self):
+        # Calculate the total tax amount
+        if self.tax_rate <= 0:
+            raise Exception("Tax rate has not been set.")
+        return self.calculate_subtotal() * self.tax_rate
+
+    def calculate_total(self):
+        # Calculate the total cost (subtotal + total tax)
+        if self._is_cart_empty():
+            return 0
+        return self.calculate_subtotal() + self.calculate_tax()
+
+    def display_cart(self):
+        # Display all items in the cart with names, prices, and quantities
+        print("-" * 50)
+        print("Items in cart:")
+        if self._is_cart_empty():
+            return
+        else:
+            for product in self.products:
+                print(f"\nProduct: {product.name:<15} Price: ${product.price:.2f}\tQty: {product.quantity}")
+
+    def _is_cart_empty(self):
+        # Return True if the cart is empty
+        if not self.products:
+            print("Cart is empty. Add some items!")
+            return True
+        return False
+
+def main():
+    # Create a ShoppingCart object
+    shopping_cart = ShoppingCart()
+
+    while True:
+        print("\nShopping Cart Menu")
+        print("1. Add product to cart")
+        print("2. Remove product from cart")
+        print("3. Update product quantity")
+        print("4. Show all items in cart")
+        print("5. Calculate total cost")
+        print("6. Set tax rate")
+        print("7. Calculate total tax")
+        print("8. Quit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Add product to cart
+            product_name = input("Enter product name: ")
+            while True:
+                try:
+                    product_price = float(input("Enter product price: "))
+                    product_quantity = int(input("Enter product quantity: "))
+                    break
+                except ValueError:
+                    print("Invalid input. Please try again.")
+
+            product = Product(product_name, product_price, product_quantity)
+            shopping_cart.add_product(product)
+
+        elif choice == '2':
+            # Remove product from cart
+            product_name = input("Enter product name to remove: ")
+            shopping_cart.remove_product(product_name)
+
+        elif choice == '3':
+            # Update product quantity
+            product_name = input("Enter product name to update: ")
+            while True:
+                try:
+                    new_quantity = int(input("Enter new quantity: "))
+                    break
+                except ValueError:
+                    print("Invalid input. Please enter a number.")
+
+            shopping_cart.update_quantity(product_name, new_quantity)
+
+        elif choice == '4':
+            # Display all items in cart
+            shopping_cart.display_cart()
+
+        elif choice == '5':
+            # Calculate and display total cost
+            total_cost = shopping_cart.calculate_total()
+            print(f"Total cost: ${total_cost:.2f}")
+
+        elif choice == '6':
+            # Set tax rate
+            while True:
+                try:
+                    tax_rate = float(input("Enter tax rate as a decimal (0.08 for 8%): "))
+                    if 0 <= tax_rate <= 1:
+                        break
+                    else:
+                        print("Tax rate must be between 0 and 1.")
+                except ValueError:
+                    print("Invalid tax rate. Please try again.")
+            shopping_cart.set_tax_rate(tax_rate)
+
+        elif choice == '7':
+            # Calculate and display total tax
+            if shopping_cart.tax_rate <= 0:
+                print("Tax rate has not been set.")
+                while True:
+                    try:
+                        tax_rate = float(input("Enter tax rate as a decimal (0.08 for 8%): "))
+                        shopping_cart.set_tax_rate(tax_rate)
+                        break
+                    except ValueError:
+                        print("Invalid tax rate. Please enter a decimal between 0 and 1.")
+
+            try:
+                total_tax = shopping_cart.calculate_tax()
+                print(f"\nTotal tax: ${total_tax:.2f}")
+            except Exception as e:
+                print(e)
+
+        elif choice == '8':
+            print("Exiting the Shopping Cart.")
+            break
+
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
---
+++ test_shopping_cart-1.py
@@ -0,0 +1,89 @@
+import unittest
+from ShoppingCart import Product  
+from ShoppingCart import ShoppingCart
+
+class TestShoppingCart(unittest.TestCase):
+    def setUp(self):
+        self.cart = ShoppingCart()
+        self.product1 = Product("Apple", 1.0, 5)
+        self.product2 = Product("Banana", 0.5, 10)
+
+    def test_add_product(self):
+        self.cart.add_product(self.product1)
+        self.assertTrue(len(self.cart.products) == 1)
+        self.assertEqual(self.cart.products[0].name, "Apple")
+        print("test_add_product: Passed")
+
+    def test_remove_product(self):
+        self.cart.add_product(self.product1)
+        self.cart.add_product(self.product2)
+        self.cart.remove_product("Apple")
+        self.assertTrue(len(self.cart.products) == 1)
+        self.assertEqual(self.cart.products[0].name, "Banana")
+        print("test_remove_product: Passed")
+
+    def test_update_quantity(self):
+        self.cart.add_product(self.product1)
+        self.cart.update_quantity("Apple", 10)
+        self.assertEqual(self.cart.products[0].quantity, 10)
+        print("test_update_quantity: Passed")
+
+    def test_calculate_subtotal(self):
+        self.cart.add_product(self.product1)
+        self.cart.add_product(self.product2)
+        self.assertEqual(self.cart.calculate_subtotal(), 10.0)
+        print("test_calculate_subtotal: Passed")
+
+    def test_set_tax_rate(self):
+        self.cart.set_tax_rate(0.08)
+        self.assertEqual(self.cart.tax_rate, 0.08)
+        print("test_set_tax_rate: Passed")
+
+    def test_calculate_tax(self):
+        self.cart.add_product(self.product1)
+        self.cart.add_product(self.product2)
+        self.cart.set_tax_rate(0.08)
+        self.assertEqual(self.cart.calculate_tax(), 0.8)
+        print("test_calculate_tax: Passed")
+
+    def test_calculate_total(self):
+        self.cart.add_product(self.product1)
+        self.cart.add_product(self.product2)
+        self.cart.set_tax_rate(0.08)
+        self.assertEqual(self.cart.calculate_total(), 10.8)
+        print("test_calculate_total: Passed")
+
+    # My tests
+    def test_is_cart_empty(self):
+        self.cart.add_product(self.product1)
+        self.cart.add_product(self.product2)
+        self.assertFalse(self.cart._is_cart_empty())
+        print("test_is_cart_empty: Passed")
+
+    def test_invalid_tax_rate(self):
+        self.assertRaises(ValueError, self.cart.set_tax_rate, 1.5)
+        print("test_invalid_tax_rate: Passed")
+
+    def test_invalid_calculate_tax(self):
+        self.assertRaises(Exception, self.cart.calculate_tax)
+        print("test_invalid_calculate_tax: Passed")
+
+    def test_no_product_found_remove(self):
+        self.cart.add_product(self.product1)
+        self.cart.add_product(self.product2)
+        self.cart.remove_product("Orange")
+        self.assertEqual(len(self.cart.products), 2)
+        print("test_no_product_found: Passed")
+
+    def test_no_product_found_update(self):
+        self.cart.add_product(self.product1)
+        qty = self.product1.quantity
+        self.cart.update_quantity("Orange", 10)
+        self.assertEqual(self.cart.products[0].quantity, qty)
+        print("test_no_product_found: Passed")
+
+    
+
+
+if __name__ == "__main__":
+    unittest.main()
